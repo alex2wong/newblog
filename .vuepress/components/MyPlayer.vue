75b5a5c57a7251b72bd402070a0d64f05b76dffa
alex2wong <huangyixiu188@hotmail.com>
2019-08-29 11:45:27 +0800

    add vudio.js to visualize Audio element

--- .vuepress/components/MyPlayer.vue
+++ .vuepress/components/MyPlayer.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    <audio :src="source" autoplay preload="auto" loop controls></audio>
+  <div class="player-cont">
+    <canvas ref="canv" width="400" height="100"></canvas>
+    <audio ref="audio" :src="source" autoplay preload="auto" loop controls></audio>
   </div>
 </template>
-<script>
-export default {
-  props: [
-    'source',
-  ],
-  mounted () {
-    
+<style scoped>
+  .player-cont {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
+  audio {
+    /* display: none; */
+  }
+</style>
+<script>
+import Vudio from 'vudio.js';
+
+export default {
+  props: {
+    source: {
+      type: String,
+    },
+  },
+  mounted () {
+    const vudio = new Vudio(this.$refs.audio, this.$refs.canv, {
+      width: 350,
+      height: 100,
+    });
+    vudio.dance();
+  },
 }
 </script>
--- posts/music.md
+++ posts/music.md
@@ -6,11 +6,15 @@ tags:
 
 ## markdown 和 vue 融合
 
-准备在markdown 的编译中，加入组件的编译，让最终的html 可以包含更丰富的内容，例如音频视频等。
+准备在markdown 的编译中，加入组件的编译，让最终的html 可以包含更丰富的内容，例如音频视频等。以下是一段 权力游戏的背景音乐，加上一个第三方音频可视化的库，可以呈现有趣的效果。
 
+<!-- <my-player source="https://music.163.com/song/media/outer/url?id=32526653.mp3" /> -->
 
-<audio src="https://music.163.com/song/media/outer/url?id=32526653.mp3" controls></audio>
+<my-player source="/got.mp3" />
+
+以下插入的是一段 iframe 内容：
 
 <iframe src="https://alex2wong.github.io/webgl_learning/webgl7/index.html"
-  style="border:none; width: 400px; height:400px; overflow:auto;"
+  style="border:none; width: 400px; height:600px; overflow:auto;
+  display:flex;align-items:center;margin-top:20px"
 />
